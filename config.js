08f5ba8239c9b2d1b6605eec6e1540f5decc733e
car123moving-prog <car123.moving@gmail.com>
2026-01-06 22:44:10 +0400

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ import {
   getFirestore
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
-// ضع إعدادات مشروعك هنا
+// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCREnC-6yJX4l1HpFVNNZgOvodBQkEri5g",
+  authDomain: "car123-moving.firebaseapp.com",
+  projectId: "car123-moving",
+  storageBucket: "car123-moving.firebasestorage.app",
+  messagingSenderId: "820235378242",
+  appId: "1:820235378242:web:94e0907a41a395c8bb17db"
 };
 
 // Initialize Firebase
