e674eff9c483861b273ca64ef60aa32edc5aed0b
car123moving-prog <car123.moving@gmail.com>
2026-01-07 08:21:41 +0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "cms-v2.1-cache";
+const ASSETS = [
+  "index.html",
+  "main.css",
+  "main.js",
+  "manifest.json",
+  "masoud-logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
