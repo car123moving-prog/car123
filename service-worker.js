11fd2390c48af7ce60b85568eb59a73e7a1c98e1
car123moving-prog <car123.moving@gmail.com>
2026-01-07 10:23:32 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,18 @@
 const CACHE_NAME = "cms-v2.1.1-cache";
-
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./main.css",
-  "./main.js",
-  "./manifest.json",
-  "./al-masaood-logo.png"
+  "index.html",
+  "main.css",
+  "main.js",
+  "manifest.json",
+  "al-masaood-logo.png"
 ];
 
-// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// ACTIVATE
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -29,7 +25,6 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// FETCH
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
