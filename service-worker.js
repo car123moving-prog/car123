d585525866968136f11e159464447e460736e97b
car123moving-prog <car123.moving@gmail.com>
2026-01-07 09:16:07 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
 const CACHE_NAME = "cms-v2.1.1-cache";
+
 const ASSETS = [
-  "index.html",
-  "main.css",
-  "main.js",
-  "manifest.json",
-  "al-masaood-logo.png"
+  "./",
+  "./index.html",
+  "./main.css",
+  "./main.js",
+  "./manifest.json",
+  "./al-masaood-logo.png"
 ];
 
+// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// ACTIVATE
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -25,6 +29,7 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+// FETCH
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
